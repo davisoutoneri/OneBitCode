62863396978521199539b61072e1fb6b93a50986
davisoutoneri <davisouto.dev@gmail.com>
2022-11-22 14:40:00 -0300

    Recursos nativos do navegador

---
+++ semana_2/dia_5/modulo_js/aula-10_recursos-nativos-do-navegador/index.js
@@ -0,0 +1,13 @@
+let nome = "Davi"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt("Insira o seu nome:")
+// let confirmacao = window.confirm("Confirma essa ação?")
+
+alert("Olá mundo!")
+prompt("Qual nome?")
+confirm("Confirma?")
