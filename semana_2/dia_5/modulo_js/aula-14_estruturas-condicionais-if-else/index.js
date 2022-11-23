36ffda8b26e8b2cd2504c9504883230e318f38f2
davisoutoneri <davisouto.dev@gmail.com>
2022-11-23 11:33:51 -0300

    Estruturas condicionais if e else

---
+++ semana_2/dia_5/modulo_js/aula-14_estruturas-condicionais-if-else/index.js
@@ -0,0 +1,17 @@
+/*
+const idade = prompt("Informe sua idade")
+
+if (idade >= 18){
+  alert("Você é maior de idade")
+} else if (idade > 12){
+  alert("Você é menor de idade")
+} else if(idade < 12){
+  alert("Você é criança")
+} else if(idade < 4){
+  alert("Você é bebê")
+}
+*/
+
+const resultado = (6 === 5) ? "Verdadeiro" : "Falso"
+
+console.log(resultado)
